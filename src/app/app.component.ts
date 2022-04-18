dc3ef412aedfd5e679d75c9c3ba0668ec84bdf12
Ajit sabat <ajit.sabat@deliverysolutions.co>
2022-04-18 13:44:48 +0530

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angular-app';
+  color = 'green';
+
+  changecolor1(data: string) {
+    this.color = data;
+  }
+
+  changecolor2(data: string) {
+    this.color = data;
+  }
 }
